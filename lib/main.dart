539bba7624e369e171571e5a24ba862e302e3f27
AaronHtlDornbirn <91245441+AaronHtlDornbirn@users.noreply.github.com>
2022-03-01 11:49:15 +0100

    Farbpalette
    Farbpalette erstellt

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'File Manager',
-      home: const LoginScreen(),
+      home: LoginScreen(),
     );
   }
 }
