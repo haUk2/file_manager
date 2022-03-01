a20886746367cb89bbbc338c4b0bd2ac871b53ee
AaronHtlDornbirn <91245441+AaronHtlDornbirn@users.noreply.github.com>
2022-03-01 11:55:20 +0100

    Farbpalette

---
+++ lib/config/palette.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/painting.dart';
+
+class Palette {
+  static const Color backgroundColorappbar = Color(0xFF90A4AE);
+  static const Color backbutton = Color(0xFF546E7A);
+  static const Color backgroundColorbody = Color(0xFFB0BEC5);
+  static const Color bodyschrift = Color(0xFF455A64);
+
+  static const Color purple1 = Color(0xFFF3E5F5);
+  static const Color purple2 = Color(0xFFE1BEE7);
+  static const Color purple3 = Color(0xFFCE93D8);
+  static const Color purple4 = Color(0xFFBA68C8);
+  static const Color purple5 = Color(0xFFAB47BC);
+  static const Color purple6 = Color(0xFF9C27B0);
+  static const Color purple7 = Color(0xFF8E24AA);
+  static const Color purple8 = Color(0xFF7B1FA2);
+  static const Color purple9 = Color(0xFF6A1B9A);
+  static const Color purple10 = Color(0xFF4A148C);
+  static const Color purpleaccent3 = Color(0xFFEA80FC);
+  static const Color purpleaccent1 = Color(0xFFE040FB);
+  static const Color purpleaccent2 = Color(0xFFD500F9);
+  static const Color purpleaccent4 = Color(0xFFAA00FF);
+}
